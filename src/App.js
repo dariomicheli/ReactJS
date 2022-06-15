b490be1d79854b46041004863a6c2b830a7a5b96
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-06-14 21:15:37 -0300

    prueba de componente navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+
+/* const App = () =>{
+  return(
+    <h1>Holis</h1>
+  );
+} */
 
 function App() {
   return (
@@ -8,6 +15,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hola Mundo!</p>
       </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,8 @@
+
+const NavBar = () =>{
+    return(
+        <h2>Hola soy el navbar</h2>
+    );
+}
+
+export default NavBar;
