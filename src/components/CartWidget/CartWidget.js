223a2218de1b941584aa9822f0481fb6dcf92151
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-06-17 17:50:35 -0300

    first commit

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,3 +1,11 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import './CartWidget.css';
+
 export default function CartWidget() {
-    return <div> CartWidget </div>;
+    return (
+        <div className='shoppingCart'>
+            <a href='/'><ShoppingCartIcon htmlColor='#fbfcff'/></a> 
+            <span>8</span> 
+        </div>
+    );
 }
