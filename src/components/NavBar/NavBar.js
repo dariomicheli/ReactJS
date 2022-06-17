9cd442e8851ab7d7ac14229996117f64b667373c
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-06-17 16:34:10 -0300

    se agregan ultimos detalles a navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,14 @@
 import './NavBar.css';
 import MenuItems from './MenuItems';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/logo.png';
 
 const NavBar = () =>{
     return(
         <div className="menu">
             <div className="menuContainer">
-                <div>
-                    <img src="" alt="" />
-                    <span>LOGO</span>
+                <div className='logo'>
+                    <a href="/" ><img src={logo} alt="logo" className='logoImg'/></a>
+                    <a href="/" className='logoName'><span>HAPPY FEET</span></a>
                 </div>
                 <MenuItems />
             </div>
