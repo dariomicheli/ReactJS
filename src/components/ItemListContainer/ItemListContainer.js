2ae5ac3314e26ed2cd9bb9897c5571da29d26b79
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-06-22 21:04:34 -0300

    se finaliza itemcount

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,14 +1,14 @@
 import ItemCount from '../ItemCount/ItemCount';
 
 export default function ItemListContainer({greeting}) {
-    const onAdd = (cantidad) => {
-        alert(`Se agregaron ${cantidad} items al carrito`);
+    const onAdd = (amount) => {
+        alert(`Se agregaron ${amount} items al carrito`);
     }
     
     return(
         <>
             <div>{greeting}</div>
-            <ItemCount stock={10} initial={1} onAdd={onAdd} />
+            <ItemCount stock={5} initial={1} onAdd={onAdd} />
         </>
     ); 
 }
