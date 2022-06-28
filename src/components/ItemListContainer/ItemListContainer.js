5c27417ef79ede5af498f358ef312c76185a00aa
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-06-28 16:25:57 -0300

    se agrega spinner

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, CSSProperties } from 'react';
 import { getData } from '../../mocks/fakeApi';
 import ItemList from '../ItemList/ItemList';
+import BeatLoader from "react-spinners/BeatLoader";
 
 export default function ItemListContainer({greeting}) {
     //funcion creada para pasar como prop en componente ItemCount
     const[productList,setProductList]=useState([]);
     const[loading,setLoading]=useState(true);
+
+    //css para spinner
+    const override: CSSProperties = {
+        display:"block",
+        width:"30",
+        margin: "0 auto",
+        textAlign:"center"
+    }
     
 /*     const onAdd = (amount) => {
         alert(`Se agregaron ${amount} items al carrito`);
@@ -20,7 +29,8 @@ export default function ItemListContainer({greeting}) {
 
     return(
         <>
-            {loading ? <p>Cargando...</p> : <ItemList productList={productList}/>}
+            <BeatLoader loading={loading} cssOverride={override} size={15} color='#36D7B7'/>
+            <ItemList productList={productList}/>
         </>
     ); 
 }
