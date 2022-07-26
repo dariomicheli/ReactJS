6db881acbc882444e28c2d4a4c41dc2f97db37cd
dariomicheli <61011993+dariomicheli@users.noreply.github.com>
2022-07-25 21:03:14 -0300

    se corrige menu

--- src/components/NavBar/MenuItems.js
+++ src/components/NavBar/MenuItems.js
@@ -2,24 +2,32 @@ import './MenuItems.css';
 import {NavLink} from 'react-router-dom';
 import { db } from "../../firebase/firebase";
 import { getDocs, collection } from "firebase/firestore";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function MenuItems() {
 
     const [items, setItems] = useState([])
 
-    const categoriesCollection = collection(db,'categories');
-    getDocs(categoriesCollection)
-    .then(result => {
-        const list = result.docs.map(categorie => {
-            return {
-                id: categorie.id,
-                ...categorie.data()
-            }
+    useEffect(() => {
+        const categoriesCollection = collection(db,'categories');
+        getDocs(categoriesCollection)
+        .then(result => {
+            const list = result.docs.map(categorie => {
+                return {
+                    id: categorie.id,
+                    ...categorie.data()
+                }
+            })
+            setItems(list.sort(sortArray));
         })
-        setItems(list);
-    })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
+    },[])
+
+    const sortArray = (x,y) => {
+        if (x.description < y.description) return -1;
+        if (x.description > y.description) return 1;
+        return 0;
+    }    
 
     return (
         <nav className="menuItems">
